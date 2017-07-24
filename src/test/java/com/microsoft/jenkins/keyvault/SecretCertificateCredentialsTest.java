062b7bb7d24aa6e4d1dac153b98dd9cbf677bc4e
Dayang Shen <dayshen@microsoft.com>
2017-07-24 10:14:42 +0800

    Add Azure KeyVault secret certificate credentials

---
+++ src/test/java/com/microsoft/jenkins/keyvault/SecretCertificateCredentialsTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE file in the project root for license information.
+ */
+
+package com.microsoft.jenkins.keyvault;
+
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.microsoft.azure.keyvault.models.SecretBundle;
+import hudson.util.Secret;
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import java.io.IOException;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+
+public class SecretCertificateCredentialsTest {
+
+    @ClassRule
+    public static JenkinsRule j = new JenkinsRule();
+
+    private static class MockCertSecretGetter implements BaseSecretCredentials.SecretGetter {
+
+        private final String cert;
+
+        private MockCertSecretGetter(String cert) {
+            this.cert = cert;
+        }
+
+        @Override
+        public SecretBundle getKeyVaultSecret(String servicePrincipalId, String secretIdentifier) {
+            Assert.assertEquals("spId", servicePrincipalId);
+            Assert.assertEquals("secretId", secretIdentifier);
+
+            final SecretBundle secretBundle = new SecretBundle();
+            secretBundle.withValue(cert);
+            secretBundle.withContentType("application/x-pkcs12");
+
+            return secretBundle;
+        }
+
+    }
+
+    @Test
+    public void getKeyStore() throws IOException, KeyStoreException, UnrecoverableKeyException, NoSuchAlgorithmException {
+        final SecretCertificateCredentials c = new SecretCertificateCredentials(
+                CredentialsScope.SYSTEM,
+                "id",
+                "desc",
+                "spId",
+                "secretId",
+                Secret.fromString("123456")
+        );
+        final String cert= IOUtils.toString(getClass().getResourceAsStream("cert.pfx.b64"), "UTF-8");
+        c.setSecretGetter(new MockCertSecretGetter(cert));
+
+        final KeyStore keyStore = c.getKeyStore();
+        Assert.assertTrue(keyStore.containsAlias("msft"));
+        Assert.assertEquals(1, keyStore.size());
+        final Key key = keyStore.getKey("msft", "123456".toCharArray());
+        Assert.assertEquals("RSA", key.getAlgorithm());
+    }
+
+}
---
+++ src/test/java/com/microsoft/jenkins/keyvault/SecretStringCredentialsTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE file in the project root for license information.
+ */
+
+package com.microsoft.jenkins.keyvault;
+
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.microsoft.azure.keyvault.models.SecretBundle;
+import hudson.util.Secret;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class SecretStringCredentialsTest {
+
+    @ClassRule
+    public static JenkinsRule j = new JenkinsRule();
+
+    @Test
+    public void getSecret() {
+        final BaseSecretCredentials.SecretGetter secretGetter = new BaseSecretCredentials.SecretGetter() {
+            @Override
+            public SecretBundle getKeyVaultSecret(String servicePrincipalId, String secretIdentifier) {
+                Assert.assertEquals("spId", servicePrincipalId);
+                Assert.assertEquals("secretId", secretIdentifier);
+
+                final SecretBundle secretBundle = new SecretBundle();
+                secretBundle.withValue("Secret");
+
+                return secretBundle;
+            }
+        };
+        final SecretStringCredentials c = new SecretStringCredentials(
+                CredentialsScope.SYSTEM,
+                "id",
+                "desc",
+                "spId",
+                "secretId"
+        );
+        c.setSecretGetter(secretGetter);
+
+        final Secret secret = c.getSecret();
+        Assert.assertEquals("Secret", secret.getPlainText());
+    }
+
+}
