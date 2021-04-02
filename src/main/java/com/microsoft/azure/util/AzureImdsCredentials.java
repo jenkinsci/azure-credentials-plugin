1ae71466f470da8f5529fb6c157f49ba5106b9ea
Tim Jacomb <timjacomb1+github@gmail.com>
2021-04-02 15:54:34 +0100

    Fix managed identity across subscriptions

--- src/main/java/com/microsoft/azure/util/AzureImdsCredentials.java
+++ src/main/java/com/microsoft/azure/util/AzureImdsCredentials.java
@@ -2,12 +2,23 @@ package com.microsoft.azure.util;
 
 import com.cloudbees.plugins.credentials.CredentialsScope;
 import com.cloudbees.plugins.credentials.impl.BaseStandardCredentials;
+import com.microsoft.azure.PagedList;
+import com.microsoft.azure.management.Azure;
+import com.microsoft.azure.management.resources.Subscription;
+import com.microsoft.jenkins.azurecommons.core.credentials.ImdsTokenCredentials;
 import com.microsoft.jenkins.azurecommons.core.credentials.TokenCredentialData;
 import hudson.Extension;
+import hudson.Util;
+import hudson.util.FormValidation;
 import hudson.util.ListBoxModel;
+import org.apache.commons.lang3.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
 
 public class AzureImdsCredentials extends AbstractManagedIdentitiesCredentials {
+
+    private String subscriptionId;
 
     public AzureImdsCredentials(CredentialsScope scope, String id, String description) {
         super(scope, id, description);
@@ -25,8 +36,41 @@ public class AzureImdsCredentials extends AbstractManagedIdentitiesCredentials {
     public TokenCredentialData createToken() {
         TokenCredentialData token = super.createToken();
         token.setType(TokenCredentialData.TYPE_IMDS);
+        token.setSubscriptionId(getSubscriptionId());
         return token;
     }
+
+    public String getSubscriptionId() {
+        return subscriptionId;
+    }
+
+    @DataBoundSetter
+    public void setSubscriptionId(String subscriptionId) {
+        this.subscriptionId = Util.fixEmpty(subscriptionId);
+    }
+
+    public boolean validate() throws AzureCredentials.ValidationException {
+        try {
+            final String credentialSubscriptionId = getSubscriptionId();
+
+            Azure.Authenticated auth = Azure.authenticate(
+                        new ImdsTokenCredentials(AzureEnvUtil.resolveAzureEnv(getAzureEnvName())));
+            PagedList<Subscription> list = auth.subscriptions().list();
+            if (subscriptionId != null) {
+                for (Subscription subscription : list) {
+                    if (subscription.subscriptionId().equalsIgnoreCase(credentialSubscriptionId)) {
+                        return true;
+                    }
+                }
+            } else {
+                return true;
+            }
+        } catch (Exception e) {
+            throw new AzureCredentials.ValidationException(Messages.Azure_CantValidate() + ": " + e.getMessage());
+        }
+        throw new AzureCredentials.ValidationException(Messages.Azure_Invalid_SubscriptionId());
+    }
+
 
     @Extension
     public static class DescriptorImpl
@@ -45,5 +89,23 @@ public class AzureImdsCredentials extends AbstractManagedIdentitiesCredentials {
             model.add(AzureEnvUtil.Constants.ENV_AZURE_US_GOVERNMENT);
             return model;
         }
+
+        public FormValidation doVerifyConfiguration(
+                @QueryParameter String subscriptionId,
+                @QueryParameter String azureEnvironmentName) {
+
+            AzureImdsCredentials imdsCredentials = new AzureImdsCredentials(null, null, null, azureEnvironmentName);
+            if (StringUtils.isNotBlank(subscriptionId)) {
+                imdsCredentials.setSubscriptionId(subscriptionId);
+            }
+            try {
+                imdsCredentials.validate();
+            } catch (AzureCredentials.ValidationException e) {
+                return FormValidation.error(e.getMessage());
+            }
+
+            return FormValidation.ok(Messages.Azure_MI_Config_Success());
+        }
+
     }
 }
